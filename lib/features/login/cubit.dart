cc7243cbba1fb7c638f36921210857d324eacb3a
AhmedAbogameel <ahmedabogameel81@gmail.com>
2023-09-03 21:48:51 +0300

    refactor login controller to login cubit

--- lib/features/login/cubit.dart
+++ lib/features/login/cubit.dart
@@ -1,13 +1,17 @@
 import 'package:dio/dio.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:note_app/core/caching_utils/caching_utils.dart';
-import 'package:note_app/core/network_utils/network_utils.dart';
-import 'package:note_app/core/route_utils/route_utils.dart';
-import 'package:note_app/features/home/view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/features/login/states.dart';
 
+import '../../core/caching_utils/caching_utils.dart';
+import '../../core/network_utils/network_utils.dart';
+import '../../core/route_utils/route_utils.dart';
 import '../../widgets/snack_bar.dart';
+import '../home/view.dart';
 
-class LoginController {
+class LoginCubit extends Cubit<LoginStates> {
+  LoginCubit() : super(LoginInit());
+
   final formKey = GlobalKey<FormState>();
   String? email, password;
 
@@ -16,6 +20,7 @@ class LoginController {
     if (!formKey.currentState!.validate()) {
       return;
     }
+    emit(LoginLoading());
     try {
       final response = await NetworkUtils.post(
         'auth/login',
@@ -36,5 +41,7 @@ class LoginController {
         error: true,
       );
     }
+    emit(LoginInit());
   }
+
 }
--- lib/features/login/view.dart
+++ lib/features/login/view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note_app/core/app_colors/app_colors.dart';
 import 'package:note_app/core/dimensions/dimensions.dart';
 import 'package:note_app/core/route_utils/route_utils.dart';
-import 'package:note_app/features/login/controller.dart';
+import 'package:note_app/features/login/cubit.dart';
+import 'package:note_app/features/login/states.dart';
 import 'package:note_app/widgets/app_app_bar.dart';
 import 'package:note_app/widgets/app_button.dart';
 import 'package:note_app/widgets/app_text.dart';
@@ -11,87 +13,82 @@ import 'package:note_app/widgets/app_text_field.dart';
 import '../../core/validator_utils/validator_utils.dart';
 import '../sign_up/view.dart';
 
-class LoginView extends StatefulWidget {
-  const LoginView({super.key});
-
-  @override
-  State<LoginView> createState() => _LoginViewState();
-}
-
-class _LoginViewState extends State<LoginView> {
-  LoginController controller = LoginController();
-  bool isLoading = false;
-
-  void toggleLoading(bool value) {
-    isLoading = value;
-    setState(() {});
-  }
+class LoginView extends StatelessWidget {
+  const LoginView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppAppBar(),
-      body: Form(
-        key: controller.formKey,
-        child: ListView(
-          padding: EdgeInsets.all(16),
-          children: [
-            SizedBox(height: 32.height),
-            AppText(
-              title: "Notes App",
-              textAlign: TextAlign.center,
-              fontSize: 40,
-              fontWeight: FontWeight.w700,
-            ),
-            SizedBox(height: 48.height),
-            AppText(
-              title: "Login",
-              textAlign: TextAlign.center,
-              fontSize: 24,
-              fontWeight: FontWeight.w500,
-            ),
-            SizedBox(height: 64.height),
-            AppTextField(
-              hint: 'Email',
-              onSaved: (v) => controller.email = v,
-              validator: ValidatorUtils.email,
-            ),
-            Divider(height: 20, color: AppColors.gray),
-            AppTextField(
-              hint: 'Password',
-              onSaved: (v) => controller.password = v,
-              validator: ValidatorUtils.password,
-            ),
-            SizedBox(height: 64.height),
-            AppButton(
-              title: 'Login',
-              isLoading: isLoading,
-              onTap: () async {
-                toggleLoading(true);
-                await controller.login(context);
-                toggleLoading(false);
-              },
-            ),
-            SizedBox(height: 48.height),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                AppText(
-                  title: 'Do not have an account?',
-                  color: AppColors.gray,
-                ),
-                SizedBox(width: 4.width),
-                AppText(
-                  title: 'Sign up',
-                  textDecoration: TextDecoration.underline,
-                  onTap: () => RouteUtils.push(
-                    context: context,
-                    view: SignUpView(),
+    return BlocProvider(
+      create: (context) => LoginCubit(),
+      child: Scaffold(
+        appBar: AppAppBar(),
+        body: Builder(
+          builder: (context) {
+            final cubit = BlocProvider.of<LoginCubit>(context);
+            return Form(
+              key: cubit.formKey,
+              child: ListView(
+                padding: EdgeInsets.all(16),
+                children: [
+                  SizedBox(height: 32.height),
+                  AppText(
+                    title: "Notes App",
+                    textAlign: TextAlign.center,
+                    fontSize: 40,
+                    fontWeight: FontWeight.w700,
                   ),
-                ),
-              ],
-            ),
-          ],
+                  SizedBox(height: 48.height),
+                  AppText(
+                    title: "Login",
+                    textAlign: TextAlign.center,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  SizedBox(height: 64.height),
+                  AppTextField(
+                    hint: 'Email',
+                    onSaved: (v) => cubit.email = v,
+                    validator: ValidatorUtils.email,
+                  ),
+                  Divider(height: 20, color: AppColors.gray),
+                  AppTextField(
+                    hint: 'Password',
+                    onSaved: (v) => cubit.password = v,
+                    validator: ValidatorUtils.password,
+                  ),
+                  SizedBox(height: 64.height),
+                  BlocBuilder<LoginCubit, LoginStates>(
+                    builder: (context, state) {
+                      return AppButton(
+                        title: 'Login',
+                        isLoading: state is LoginLoading,
+                        onTap: () => cubit.login(context),
+                      );
+                    },
+                  ),
+                  SizedBox(height: 48.height),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      AppText(
+                        title: 'Do not have an account?',
+                        color: AppColors.gray,
+                      ),
+                      SizedBox(width: 4.width),
+                      AppText(
+                        title: 'Sign up',
+                        textDecoration: TextDecoration.underline,
+                        onTap: () => RouteUtils.push(
+                          context: context,
+                          view: SignUpView(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          }
         ),
       ),
     );
