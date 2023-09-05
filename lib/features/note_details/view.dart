d56b55e797eed10d0a5bd88dd082c15d256ad998
AhmedAbogameel <ahmedabogameel81@gmail.com>
2023-09-05 20:36:39 +0300

    get note

--- lib/features/note_details/view.dart
+++ lib/features/note_details/view.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:note_app/core/dimensions/dimensions.dart';
+import 'package:note_app/widgets/app_loading_indicator.dart';
 
-import '../../core/models/note.dart';
-import '../../core/route_utils/route_utils.dart';
 import '../../widgets/app_app_bar.dart';
 import '../../widgets/app_icon_button.dart';
 import '../../widgets/app_text.dart';
-import '../note_editor/view.dart';
+import 'cubit.dart';
 
 class NoteDetailsView extends StatelessWidget {
   const NoteDetailsView({super.key, required this.id});
@@ -16,38 +16,55 @@ class NoteDetailsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppAppBar(
-        actions: [
-          AppIconButton(
-            icon: FontAwesomeIcons.penToSquare,
-            onTap: () async {
-              // final result = await RouteUtils.push(
-              //   NoteEditorView(note: note),
-              // );
-              // if (result != null) {
-              //   Navigator.pop(context, result);
-              // }
-            },
-          ),
-          SizedBox(width: 16),
-        ],
-        enableBackButton: true,
-      ),
-      body: ListView(
-        padding: EdgeInsets.all(16),
-        children: [
-          AppText(
-            title: "note.title",
-            fontSize: 36,
-            fontWeight: FontWeight.w600,
-          ),
-          SizedBox(height: 16.height),
-          AppText(
-            title: "note.subtitle",
-            fontSize: 24,
-          ),
-        ],
+    return BlocProvider(
+      create: (context) => NoteDetailsCubit(id: id)..getNoteDetails(),
+      child: BlocBuilder<NoteDetailsCubit, NoteDetailsStates>(
+        builder: (context, state) {
+          final cubit = BlocProvider.of<NoteDetailsCubit>(context);
+          return Scaffold(
+            appBar: AppAppBar(
+              actions: [
+                if (state is NoteDetailsInit)
+                AppIconButton(
+                  icon: FontAwesomeIcons.penToSquare,
+                  onTap: () async {
+                    // final result = await RouteUtils.push(
+                    //   NoteEditorView(note: note),
+                    // );
+                    // if (result != null) {
+                    //   Navigator.pop(context, result);
+                    // }
+                  },
+                ),
+                SizedBox(width: 16),
+              ],
+              enableBackButton: true,
+            ),
+            body: Builder(
+              builder: (context) {
+                if (state is NoteDetailsLoading) {
+                  return AppLoadingIndicator();
+                }
+                final note = cubit.note!;
+                return ListView(
+                  padding: EdgeInsets.all(16),
+                  children: [
+                    AppText(
+                      title: note.title,
+                      fontSize: 36,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    SizedBox(height: 16.height),
+                    AppText(
+                      title: note.subtitle,
+                      fontSize: 24,
+                    ),
+                  ],
+                );
+              }
+            ),
+          );
+        },
       ),
     );
   }
